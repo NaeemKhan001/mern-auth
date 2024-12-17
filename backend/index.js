b8de94c0f26c4f5c0a6fef89b33b49e98e674daa
NaeemKhan001 <naeem.ullah98@hotmail.com>
2024-12-17 18:31:16 +0500

    Database connection

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
+mongoose
+  .connect(`${process.env.MONGODB_URI}`)
+  .then(() => {
+    console.log("Connnected to DB");
+  })
+  .catch((error) => {
+    console.log("error connnecting to DB", error);
+  });
 
 app.listen(4000, () => {
   console.log(`Server listening on port 4000`);
